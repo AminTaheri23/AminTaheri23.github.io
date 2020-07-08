1fc0a354e641d84ecf8dae7fe5bfafe2401794b2
Amin Taheri <30533823+AminTaheri23@users.noreply.github.com>
2020-07-09 01:56:17 +0430

    image 3:2 ratio + button

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,26 @@
-$(function () {
-    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('6000');
+            $('.filter').filter('.'+value).show('6000');
+            
+        }
     });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
